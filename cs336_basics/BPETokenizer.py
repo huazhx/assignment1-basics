5d9d43bf7eac02f182425c3172fd68af6f51201f
huazhx <728820587@qq.com>
2025-08-20 00:37:22 +0800

    Refactor BPETokenizer

--- cs336_basics/BPETokenizer.py
+++ cs336_basics/BPETokenizer.py
@@ -1,7 +1,7 @@
 import regex as re
 import multiprocessing
 from collections import defaultdict
-from pretokenization_example import find_chunk_boundaries
+from cs336_basics.pretokenization_example import find_chunk_boundaries
 
 
 class BPETokenizer:
@@ -24,8 +24,11 @@ class BPETokenizer:
         self.end_of_text = "<|endoftext|>"
         self.encoding = 'utf-8'
 
-        self.special_tokens.append(self.end_of_text)
+        if not self.end_of_text in self.special_tokens:
+            self.special_tokens.append(self.end_of_text)
         self.next_id: int = 0
+
+        self._init_vocabulary()
 
 
     def _init_vocabulary(self):
@@ -39,7 +42,7 @@ class BPETokenizer:
             self.vocab[i] = byte_val
             self.inv_vocab[byte_val] = i
 
-        self.next_id = 255
+        self.next_id = 256
         for token in self.special_tokens:
             token_bytes = token.encode(self.encoding)
             self.vocab[self.next_id] = token_bytes
@@ -58,44 +61,59 @@ class BPETokenizer:
         """
         frequency_map = {}
 
-        matches = re.findall(self.PAT, chunk)
+        for st in self.special_tokens:
+            chunk = chunk.replace(st, "")
+
+        matches = re.findall(self.PAT, chunk.strip())
         for match in matches:
             bytes_tuple = tuple(
                 [bytes(e, encoding=self.encoding) for e in match])
             frequency_map[bytes_tuple] = frequency_map.get(bytes_tuple, 0)
+            frequency_map[bytes_tuple] += 1
 
         return frequency_map
 
-    def _pre_tokenize(self):
+    def _pre_tokenize(self, debug = False):
         """
         Returns:
             Dictionary mapping byte tuples to their frequencies across all text 
             For example:
 
         """
-        with open(self.input_path, "rb") as f:
-            num_processes = self.num_processes
-            boundaries = find_chunk_boundaries(
-                f, num_processes, b"<|endoftext|>")
-            chunks = []
 
-            # The following is a serial implementation, but you can parallelize this
-            # by sending each start/end pair to a set of processes.
-            for start, end in zip(boundaries[:-1], boundaries[1:]):
-                f.seek(start)
-                chunk = f.read(end - start).decode("utf-8", errors="ignore")
-                # Run pre-tokenization on your chunk and store the counts for each pre-token
-                chunks.append(chunk)
+        if not debug:
+            with open(self.input_path, "rb") as f:
+                num_processes = self.num_processes
+                boundaries = find_chunk_boundaries(
+                    f, num_processes, b"<|endoftext|>")
+                chunks = []
 
-            with multiprocessing.Pool(self.num_processes) as pool:
-                frequency_results = pool.map(self._pre_tokenize_worker, chunks)
+                # The following is a serial implementation, but you can parallelize this
+                # by sending each start/end pair to a set of processes.
+                for start, end in zip(boundaries[:-1], boundaries[1:]):
+                    f.seek(start)
+                    chunk = f.read(end - start).decode("utf-8", errors="ignore")
+                    # Run pre-tokenization on your chunk and store the counts for each pre-token
+                    chunks.append(chunk)
 
+                with multiprocessing.Pool(self.num_processes) as pool:
+                    frequency_results = pool.map(self._pre_tokenize_worker, chunks)
+
+                combined_frequencies = defaultdict(int)
+                for fre_map in frequency_results:
+                    for bytes_tuple, freq in fre_map.items():
+                        combined_frequencies[bytes_tuple] += freq
+
+                return dict(combined_frequencies)
+        else:
             combined_frequencies = defaultdict(int)
-            for fre_map in frequency_results:
-                for bytes_tuple, freq in fre_map.item():
+            with open(self.input_path, "rb") as f:
+                content = f.read()
+                fm = self._pre_tokenize_worker(chunk=content.decode("utf-8", errors="ignore"))
+                for bytes_tuple, freq in fm.items():
                     combined_frequencies[bytes_tuple] += freq
 
-            return dict(combined_frequencies)
+                return dict(combined_frequencies)
 
     def _count_pairs(self, word_freqs: dict[tuple[bytes, bytes], int]):
         """
@@ -129,7 +147,7 @@ class BPETokenizer:
             current = bytes()
             for j in range(i, len(bytes_t)):
                 current += bytes_t[j]
-                if not current in self.vocab:
+                if not current in self.inv_vocab:
                     results.append(current[:-1])
                     break
         return results
@@ -143,32 +161,23 @@ class BPETokenizer:
             pair_freq: The byte pairs with frequency to merge
 
         """
-        if pair_freq < 2:
-            return pair_freq
-        sorted_pairs = sorted(pair_freq.items(), key=lambda x: (x[1], x[0][0], x[0][1]))
+        if len(pair_freq) < 2:
+            return
+        sorted_pairs = sorted(pair_freq.items(), key=lambda x: (x[1], x[0][0], x[0][1]), reverse=True)
         target = sorted_pairs[0]
 
-        # target = sorted_pairs[0][1] - 1
-        # left, right, mid = 0, len(sorted_pairs) - 1, -1
-        # while left < right:
-        #     mid = (left + right) // 2
-        #     if sorted_pairs[mid][0] > target:
-        #         left = mid
-        #     elif sorted_pairs[mid][0] < target:
-        #         right = mid
-        #     else:
-        #         break
-
-        self.vocab[target[0][0] + target[0][1]] += target[1]
+        new_bytes_pair = target[0][0] + target[0][1]
+        self.vocab[self.next_id] = new_bytes_pair
+        self.inv_vocab[new_bytes_pair] = self.next_id
         self.next_id += 1
         self.merges.append(target[0])
         
     def train(self):
-        self._init_vocabulary()
-        bytes_freq = self._pre_tokenize()
-
-        while self.next_id < self.vocab_size:
+        bytes_freq = self._pre_tokenize(debug=True)
+        real_id = self.next_id
+        while real_id < self.vocab_size:
             pair_freq = self._count_pairs(bytes_freq)
             self._merge_vocab(pair_freq)
-
+            real_id += 1
         return 
+
--- tests/adapters.py
+++ tests/adapters.py
@@ -589,4 +589,10 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+
+    # raise NotImplementedError
+
+    from cs336_basics.BPETokenizer import BPETokenizer
+    bpe = BPETokenizer(input_path=input_path, vocab_size=vocab_size, special_tokens=special_tokens)
+    bpe.train()
+    return (bpe.vocab, bpe.merges)
